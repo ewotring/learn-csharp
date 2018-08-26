34cd36d5ca62fcb75c69de4fc9eaa0fe89841148
Erik Wotring <ewotring@gmail.com>
2018-08-26 11:44:21 -0500

    Run initial build and run

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeleprompterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
