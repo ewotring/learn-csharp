af33dddf6b3941dfbab1dcda477cd897a7a080ac
Erik Wotring <ewotring@gmail.com>
2018-08-26 12:51:57 -0500

    Add async tasks

--- Program.cs
+++ Program.cs
@@ -7,23 +7,69 @@ namespace TeleprompterConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        private static async Task RunTeleprompter()
         {
-            Console.WriteLine("Hello World!");
-            var lines = ReadFrom("sampleQuotes.txt");
-            foreach (var line in lines)
+            var config = new TelePrompterConfig();
+            var displayTask = ShowTeleprompter(config);
+
+            var speedTask = GetInput(config);
+            await Task.WhenAny(displayTask, speedTask);
+        }
+
+        private static async Task ShowTeleprompter(TelePrompterConfig config)
+        {
+            var words = ReadFrom("sampleQuotes.txt");
+            foreach (var line in words)
             {
-                // Console.WriteLine(line);
                 Console.Write(line);
                 if (!string.IsNullOrWhiteSpace(line))
                 {
-                    var pause = Task.Delay(200);
-                    // Synchronously waiting on a task is an
-                    // anti-pattern. This will get fixed in later
-                    // steps.
-                    pause.Wait();
+                    // await Task.Delay(200);
+                    await Task.Delay(config.DelayInMilliseconds);
                 }
             }
+            config.SetDone();
+        }
+
+        private static async Task GetInput(TelePrompterConfig config)
+        {
+            // var delay = 200;
+            Action work = () =>
+            {
+                do {
+                    var key = Console.ReadKey(true);
+                    if (key.KeyChar == '>')
+                        config.UpdateDelay(-10);
+                    // {
+                    //     delay -= 10;
+                    // }
+                    else if (key.KeyChar == '<')
+                        config.UpdateDelay(10);
+                    // {
+                    //     delay += 10;
+                    // }
+                } while (!config.Done);
+            };
+            await Task.Run(work);
+        }
+        static void Main(string[] args)
+        {
+            RunTeleprompter().Wait();
+            // Console.WriteLine("Hello World!");
+            // var lines = ReadFrom("sampleQuotes.txt");
+            // foreach (var line in lines)
+            // {
+            //     // Console.WriteLine(line);
+            //     Console.Write(line);
+            //     if (!string.IsNullOrWhiteSpace(line))
+            //     {
+            //         var pause = Task.Delay(200);
+            //         // Synchronously waiting on a task is an
+            //         // anti-pattern. This will get fixed in later
+            //         // steps.
+            //         pause.Wait();
+            //     }
+            // }
         }
 
         static IEnumerable<string> ReadFrom(string file)
